9d571103fa78de42c4a549e8cdf3661698ff0d0c
Alumno <Alumno@PC28>
2019-09-24 09:47:02 -0500

    F

---
+++ WpfApp11/WpfApp11/Empleado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp11
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+
+        public string Titulo { get; set; }
+
+
+        public static Empleado GetEmpleado()
+        {
+            var emp = new Empleado()
+            {
+                Nombre = "Hugo Ramos",
+                Titulo = "Experto en Drogas"
+            };
+            return emp;
+        }
+
+
+
+ 
+    }
+}
